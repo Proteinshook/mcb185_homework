35cd954c42463b5c0cc996c6cdd08f2d2f43b301
Proteinshook <ekaidjou@gmail.com>
2024-01-24 17:23:56 -0800

    adjusting style on oligo

--- 22oligotemp.py
+++ 22oligotemp.py
@@ -4,7 +4,7 @@ def oligo(A, T, G, C):
 	if A + T + G + C <= 13:
 		return (A + T) * 2 + (G + C) * 4
 	else: 
-		return (64.9 + 41*(G + C - 16.4)) / (A + T + G + C)
+		return (64.9 + 41 * (G + C - 16.4)) / (A + T + G + C)
 
 print(oligo(1, 2, 2, 2))
 print(oligo(15, 100, 24, 62))
