08d9cc3267040a7e93bf72e2a0ab2885a0fe8d27
Proteinshook <ekaidjou@gmail.com>
2024-02-06 09:46:18 -0800

    update

--- 38quiz.py
+++ 38quiz.py
@@ -10,4 +10,23 @@ def greg(n):
 	end = end * 4
 	return(end)
 
-print(greg(100))
+
+
+def nila(n):
+	n1 = 2
+	pi = 3
+	sign = 1
+	for x in range(1, n + 1):
+		n3 = n1 + 2
+		n2 = n1 + 1
+		mult = (n1 * n2 * n3)
+		n1 = n3
+		pi = pi + (sign * 4 / mult)
+		sign = sign * -1
+	return(pi)
+
+def task(n):
+	for x in range(90, n+1):
+		print(nila(x), greg(x))
+		
+task(100)
