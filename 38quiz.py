9837d233907e8ce75c526870791f41725f7cb85c
Proteinshook <ekaidjou@gmail.com>
2024-02-06 09:32:34 -0800

    finished task 1

--- 38quiz.py
+++ 38quiz.py
@@ -1,2 +1,13 @@
 # 38quiz.py Ethan Djou Coauthored: Khalid Saif, George Mo, Jordan S
 
+def greg(n):
+	sign = -1
+	end = 0
+	for x in range(0, n + 1):
+		sign = sign * -1
+		equation = (sign)/(2*x + 1)
+		end = end + equation
+	end = end * 4
+	return(end)
+
+print(greg(100))
