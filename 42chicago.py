be772ef48b8def488ae81047c8ebdb6a29fa27be
Proteinshook <ekaidjou@gmail.com>
2024-02-09 23:48:52 -0800

    updated chicago

--- 42chicago.py
+++ 42chicago.py
@@ -1,11 +1,28 @@
+# 42chicago.py
 import random 
+import sys
 
-for i in range(3000):
+"""
+games = 1000
+for i in range(games):
 	score = 0
-	for roundnumber in range(2, 13):
-		d1 = random.randint(1, 6)
-		d2 = random.randint(1, 6)
-		if d1+d2 == roundnumber:
-			score += roundnumber
-			
-	print(score)
+	for target in range(2, 13):
+		if random.randint(1, 6) + random.randint(1, 6) == target:
+			score += target
+	print(score) # final game score
+"""
+
+games = 1000000 # 1 million trials
+log = games // 100 # report progress at 1% intervals
+total = 0
+zeroes = 0
+for i in range(games):
+	if i % log == 0: print(f'{100 * i/games:.0f}')
+	score = 0
+	for target in range(2, 13):
+		if random.randint(1, 6) + random.randint(1, 6) == target:
+			score += target
+	if score == 0: zeroes += 1
+	total += score
+print(total / games)
+print(zeroes / games)
