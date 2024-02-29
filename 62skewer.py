792c0621f783c602bd40dbe720d5cf6f90d0fb94
Proteinshook <ekaidjou@gmail.com>
2024-02-29 09:45:26 -0800

    optimized 62

--- 62skewer.py
+++ 62skewer.py
@@ -19,19 +19,20 @@ for defline, seq in mcb185.read_fasta(path):
 	gcskew = dogma.gc_skew(window)
 	gnum = window.count('G')
 	cnum = window.count('C')
-	print((f'{0}\t{gccomp:.3f}\t{gcskew:.3f}'))
-	for i in range(1, len(seq) - w + 1):
-		drop = window[0]
-		window.pop(0)
-		if i + w < len(seq):
-			window.append(seq[i+w])
-		pickup = window[-1]
-		if drop in 'G': gnum -= 1
-		if drop in 'C': cnum -= 1
-		if pickup in 'G': gnum += 1
-		if pickup in 'C': cnum += 1
-		gccomp2 = (gnum + cnum) / len(window)
-		gcskew2 = (gnum - cnum) / (gnum + cnum)
+	
+	for i in range(len(seq) - w):
+		drop   = seq[i]
+		pickup = seq[i + w]
+		
+		if   drop == 'G':   gnum -= 1
+		elif drop == 'C':   cnum -= 1
+		
+		if   pickup == 'G': gnum += 1
+		elif pickup == 'C': cnum += 1
+		
+		gccomp2 = (gnum + cnum) / w
+		if (gnum + cnum) > 0: gcskew2 = (gnum - cnum) / (gnum + cnum)
+		else:                 gcskew2 = 0
 		print(f'{i}\t{gccomp2:.3f}\t{gcskew2:.3f}')
 
 
