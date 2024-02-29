7057b4dc91ce3149b9a43a8fa732ce3b056e421b
Proteinshook <ekaidjou@gmail.com>
2024-02-29 12:38:50 -0800

    fixed 46 repeating code

--- 46savingthrows.py
+++ 46savingthrows.py
@@ -4,48 +4,28 @@
 import random
 
 
+def prob(trials, type):
+	print(type)
+	for i in range(5, 16, 5):
+		print(i, end='\t')
+		success = 0
+		for n in range(trials):
+			d1 = random.randint(1, 20)
+			d2 = random.randint(1, 20)
+			if d1 > d2: 
+				a = d1
+				b = d2
+			else: 
+				a = d2
+				b = d1
+			c = d1
+			if type == 'advantage': dice = a
+			if type == 'disadvantage': dice = b
+			if type == 'normal': dice = c
+			if dice >= i:
+				success += 1
+		print(success / trials)
 
-
-trials = 1000
-
-print('advantage')
-for i in range(5, 16, 5):
-	print(i, end='\t')
-	success = 0
-	for n in range(trials):
-		d1 = random.randint(1, 20)
-		d2 = random.randint(1, 20)
-		if d1 > d2: 
-			a = d1
-		else: 
-			a = d2
-		if a >= i:
-			success += 1
-	print(success / trials)
-print('\ndisadvantage')
-
-for i in range(5, 16, 5):							
-	print(i, end='\t')
-	success = 0
-	for n in range(trials):
-		d1 = random.randint(1, 20)
-		d2 = random.randint(1, 20)
-		if d1 < d2:
-			a = d1
-		else:
-			a = d2
-		if a >= i:
-			success += 1
-	print(success / trials)
-print('\nnormal')
-
-for i in range(5, 16, 5):						
-	print(i, end='\t')
-	success = 0
-	for n in range(trials):
-		d = random.randint(1, 20)
-		if d >= i:
-			success += 1
-	print(success / trials)
-	
-	
+prob(1000, 'advantage')
+prob(1000, 'disadvantage')
+prob(1000, 'normal')
